72e83629c362e084d86bffba8acbb9eb8edcf89a
NoyonAhammadKhan <noyonahammadkhan6464@gmail.com>
2022-10-25 03:11:33 -0400

    course details done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express=require('express');
 const app=express();
-const cors=require('cors')
+const cors=require('cors');
+const port = process.env.PORT || 5000;
 
 const courses = require('./data/courses.json')
 app.use(cors())
@@ -12,6 +13,12 @@ app.get('/courses',(req,res)=>{
     res.send(courses)
 })
 
-app.listen(5000,()=>{
+app.get('/course/:id',(req,res)=>{
+    const id = req.params.id;
+    const selectCourse = courses.find(course=>course._id===id);
+    res.send(selectCourse);
+})
+
+app.listen(port,()=>{
     console.log('app is listening')
 })
