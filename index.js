5e0714984311bfa4bdda2c0877804733e5da71e1
NoyonAhammadKhan <noyonahammadkhan6464@gmail.com>
2022-10-27 10:29:42 -0400

    edit in json file

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express=require('express');
-const app=express();
 const cors=require('cors');
+const courses = require('./data/courses.json');
+
+
+const app=express();
 const port = process.env.PORT || 5000;
 
-const courses = require('./data/courses.json');
+
 app.use(cors())
 app.get('/',(req, res)=>{
     res.json('hello world hello')
@@ -22,3 +25,17 @@ app.get('/course/:id',(req,res)=>{
 app.listen(port,()=>{
     console.log(`app is running ${port}`)
 })
+
+
+
+// const express = require('express')
+// const app = express()
+// const port = 5000
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening on port ${port}`)
+// })
